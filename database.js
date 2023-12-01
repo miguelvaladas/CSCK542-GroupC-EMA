9e56cdd041bd88914aa967de7979e98f29cffe6b
Christine Parry <114811596+christineparry9@users.noreply.github.com>
2023-12-01 16:21:42 +0100

    .

--- database.js
+++ database.js
@@ -2,8 +2,8 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config()
 
-//initialize the database connection
 const pool = mysql.createPool({
+
   host: '127.0.0.1',
   user: 'root',
   password: 'testtest',
